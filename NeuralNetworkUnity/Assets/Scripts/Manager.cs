c41f480e399e75ce5c35904a31e471005452bc52
Myron <myron.antonissen@gmail.com>
2022-05-05 16:39:26 +0200

    Adding a manager

---
+++ NeuralNetworkUnity/Assets/Scripts/Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public int MaxPopulation = 10;
+    public int SimulationTime = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxPopulation = 10;
+        SimulationTime = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Create ships
+        CreateShips();
+
+        //Filter best ones
+        //Mutate
+
+    }
+
+    void CreateShips()
+    {
+
+    }
+}
